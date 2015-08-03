2c556c3e48c7a2633dae636bf5b25797eeae148a
root <beevageeva@gmail.com>
2015-08-03 09:36:20 +0200

    added vacation

--- mail_config_paths.py
+++
@@ -1,3 +0,0 @@
-FORWARD_FILE="/home/forward/$username"
-FORWARD_PROCMAIL_FILE="/home/forward_procmail/$username"
-FORWARD_MSG_FILE="/home/forward_msg/$username"
--- prog_config_paths.py
+++
@@ -1,2 +0,0 @@
-PROCMAIL_PATH="/usr/bin/procmail"
-SENDMAIL_PATH="/usr/sbin/sendmail"
