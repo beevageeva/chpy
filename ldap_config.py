42448105e8c6f48cb60bc967ce939ff432de360c
root <beevageeva@gmail.com>
2015-08-21 09:19:17 +0200

    last

--- ldap_config.py
+++
@@ -1,3 +0,0 @@
-SERVER_NAME = "ferrari.cca.iac.es"
-BASE_USER = "ou=People,dc=cca,dc=iac,dc=es"
-USER_ATTR = "uid"
--- vacation_config_paths.py
+++
@@ -1,4 +0,0 @@
-FORWARD_FILE="/home/forward/$username"
-#this cannot be changed as vacation program will read this file which cannot be passed as param
-FORWARD_MSG_FILE="/home/$username/.vacation.msg"
-VACATION_PATH="/usr/bin/vacation"
